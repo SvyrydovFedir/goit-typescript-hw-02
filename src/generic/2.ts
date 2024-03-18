5dd4dfed64671dbe4286e95fdce0de0e6bf9bc83
SvyrydovFedir <118449931+SvyrydovFedir@users.noreply.github.com>
2024-03-18 03:28:07 +0200

    complete tasks

---
+++ src/generic/2.ts
@@ -0,0 +1,27 @@
+/*
+  У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType. 
+  Ваше завдання – використовувати Pick та generics для вказівки, що поля цих об'єктів належать AllType.
+  Функція compare повинна повертати AllType.
+*/
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+type TopInfo = Pick<AllType, "name" | "color">;
+type BottomInfo = Pick<AllType, "position" | "weight">;
+function compare<T extends TopInfo, K extends BottomInfo>(
+  top: T,
+  bottom: K
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+export {};
